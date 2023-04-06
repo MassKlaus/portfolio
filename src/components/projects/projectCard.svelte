1229b148c6f71602a7c8402ce5c5e89785775e94
MassKlaus <massinemouha@zohomail.com>
2023-04-06 15:35:09 +0000

    created a component to display projects and added current completed projects to the list

---
+++ src/components/projects/projectCard.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	export let image: string;
+	export let title: string;
+	export let description: string;
+	export let link: string;
+</script>
+
+<!--open in new tab-->
+<a href="{link}" target="_blank" class="project-card">
+	<div class="card-image" style="background-image: url({image})">
+		<div class="gradient-overlay">
+			<h3>{title}</h3>
+			<p>{description}</p>
+		</div>
+	</div>
+</a>
+
+<style lang="scss">
+	.project-card {
+		position: relative;
+		width: 100%;
+		max-width: rem(350);
+		overflow: hidden;
+		border-radius: rem(5);
+
+		$shadow-blur: rem(2);
+
+		color: $text-color;
+		box-shadow: 0 rem(0) $shadow-blur $highlight-color;
+
+		transition: all 0.25s ease;
+
+		cursor: pointer;
+
+		&:hover {
+			transform: translateY(rem(-5));
+			box-shadow: rem(0) rem(5) calc(#{$shadow-blur} * 10) rem(2) $highlight-color;
+		}
+
+		background-color: $primary-color;
+
+		.card-image {
+			height: rem(200);
+			background-size: cover;
+			background-position: center;
+			background-repeat: no-repeat;
+
+			.gradient-overlay {
+				position: absolute;
+				bottom: 0;
+				left: 0;
+				width: 100%;
+				background: linear-gradient(to top, rgba(26, 26, 62, 0.9), transparent);
+				display: flex;
+				flex-direction: column;
+				justify-content: flex-end;
+				padding: 1rem;
+				// opacity: 0;
+				transform: translateY(59%);
+				transition: all 0.5s ease;
+
+				//adding text-shadow to the h3 and p elements
+				h3 {
+					font-weight: bold;
+					margin: 0 0 0.5rem;
+					text-shadow: rem(2) rem(2) 0 $highlight-color, rem(-2) rem(-2) 0 $accent-color;
+				}
+
+				p {
+					font-family: Poppins, Roboto, sans-serif;
+					margin: 0;
+					height: rem(80);
+					text-shadow: rem(2) rem(2) 0 $highlight-color;
+				}
+			}
+
+			&:hover {
+				.gradient-overlay {
+					transform: translateY(0%);
+				}
+			}
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,223 +1,117 @@
 <script lang="ts">
-	import { onInterval } from '../utils/timer';
+	import HeroBanner from '../components/home/heroBanner.svelte';
+	import ProjectCard from '../components/projects/projectCard.svelte';
+	import type { Project } from '../types/projects';
 
-	const bannerTextOptions = ['Business', 'Reach', 'Network', 'Influence'];
-	const printTextInterval = 100;
-	const printCursorInterval = 250;
-	const freezeDuration = 15;
-
-	let bannerTextIndex = 0;
-	let bannerTextToDisplay = bannerTextOptions[0];
-	let bannerText = bannerTextToDisplay;
-	let forwardPrint = true;
-	let freezeFrameCount = 0;
-	let showCursor = false;
-
-	$: cursor = showCursor ? '|' : '';
-
-	function AnimateBannerText() {
-		if (bannerText.length === 0 && !forwardPrint) {
-			forwardPrint = true;
-			bannerTextIndex = ++bannerTextIndex % bannerTextOptions.length;
-			bannerTextToDisplay = bannerTextOptions[bannerTextIndex];
-			freezeFrameCount = 0;
+	let projects: Project[] = [
+		{
+			title: 'Yapped - Metty/Klaus',
+			image: '/raw-images/metty-yapped.png',
+			description:
+				'A Paramater editor for Sekiro: Shadows Die Twice allowing to edit in game behaviors',
+			link: 'https://github.com/MassKlaus/Yapped-Klaus'
+		},
+		{
+			title: 'Text Parser',
+			image: '/raw-images/textParser-transparent.png',
+			description:
+				'A text parser that takes a file input and converts tokens and keywords into specific symbols',
+			link: 'https://github.com/MassKlaus/TextParser'
+		},
+		{
+			title: 'Skin Furyu Manager',
+			image: '/raw-images/SkinFuryu_Transparent_logo.png',
+			description:
+				'Soap recipe manager and ingredient classing tool with pdf export and costing calculation',
+			link: 'https://github.com/MassKlaus/SkinFuryu_Cost_Manager'
 		}
-
-		if (bannerTextToDisplay.length === bannerText.length) {
-			if (freezeDuration >= freezeFrameCount) {
-				freezeFrameCount++;
-				return;
-			}
-
-			forwardPrint = false;
-		}
-
-		if (forwardPrint) {
-			bannerText = bannerTextToDisplay.substring(0, bannerText.length + 1);
-		} else {
-			bannerText = bannerText.substring(0, bannerText.length - 1);
-		}
-	}
-
-	function blinkCursor() {
-		showCursor = !showCursor;
-	}
-	onInterval(AnimateBannerText, printTextInterval);
-	onInterval(blinkCursor, printCursorInterval);
+	];
 </script>
 
-<div class="banner" id="home">
-	<div class="content">
-		<div class="text-banner">
-			<h3 class="title-text">
-				Grow your <span class="catchWord">{bannerText}!</span>
-			</h3>
-			<h4 class="subtitle-text">A new audience a few clicks away</h4>
-			<a class="call-to-action" href="#contact">Get in Touch</a>
-		</div>
-		<div class="banner-scroll-indicator">
-			<span />
-			<span />
-			<span />
-		</div>
+<HeroBanner />
+
+<section class="info-section" id="projects">
+	<h1 class="section-title">Projects</h1>
+	<div class="projects">
+		{#each projects as project (project.title)}
+			<ProjectCard {...project} />
+		{/each}
 	</div>
-</div>
+</section>
 
-<style lang="scss" scoped>
-	@use 'sass:math';
+<section class="info-section" id="about">
+	<h1 class="section-title">About Me</h1>
+	<div class="about-content">
+		<div class="info-wrapper">
+			<p>
+				Hello! My name is Massine Mouha, a 21-year-old Moroccan software developer who is passionate
+				about coding and technology. With 4 years of freelance experience under my belt, I have
+				worked on various projects, ranging from Facebook scrapers to desktop app development and
+				mobile development. I am well-versed in multiple programming languages and frameworks,
+				including advanced C#, TypeScript, Rust, React Native, React, Next.js, Nuxt, Vue, and
+				Svelte.
+			</p>
+			<p>
+				I hold a license in software development and am currently furthering my education to broaden
+				my knowledge and skillset in the industry. Despite being a front-end enthusiast, my true
+				passion lies in back-end development. I find great satisfaction in creating efficient,
+				reliable, and scalable back-end systems.
+			</p>
+			<p>
+				When I'm not coding, I enjoy working out, gaming, and exploring new technologies, as coding
+				itself is my prime hobby. I am a firm believer in continuous learning and personal growth,
+				striving to improve my skills and adapt to the ever-evolving world of software development.
+			</p>
+			<p>
+				If you're interested in collaborating on a project or just want to say hello, please feel
+				free to <a href="#contact">get in touch!</a>
+			</p>
+		</div>
+		<blockquote>
+			<p>"Give a man a program, frustrate him for a day. Teach a man to program, frustrate him for a lifetime."</p> -- <i>Muhammad Waseem</i>
+		</blockquote>
+	</div>
+</section>
 
-	.banner {
-		position: relative;
-		background-image: url(/images/banner-glitch.jpg);
-		background-size: cover;
-		background-position: center;
-		height: 100svh; /* Set the height of the banner */
+<div style="height: 100vh;" />
+
+<style lang="scss">
+	.projects {
 		display: flex;
-		justify-content: center;
-		align-items: center;
-		text-align: center;
-		color: #fff;
+		gap: rem(20);
+		flex-wrap: wrap;
+		padding: 0 rem(20);
 
-		box-shadow: rem(0) rem(1) rem(5) $accent-color, rem(0) rem(2) rem(10) $highlight-color;
-
-		z-index: 0;
-
-		&::after {
-			content: '';
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%;
-			background-color: rgba(0, 0, 0, 0.5);
+		@include mobile {
+			align-items: center;
 		}
 
-		.content {
-			z-index: 1;
+		@include tablet {
+			justify-content: center;
+		}
+	}
 
-			padding: 0 rem(10);
+	.about-content {
+		display: flex;
+		justify-content: space-evenly;
+		flex-wrap: wrap;
+		align-items: center;
 
-			.text-banner {
-				display: flex;
-				flex-direction: column;
+		.info-wrapper {
+			max-width: 75ch;
 
-				align-items: center;
-				.title-text {
-					font-size: rem(56);
-					margin-bottom: 0;
-					.catchWord {
-						display: inline-block;
-						min-width: 5ch;
-						text-align: left;
-						color: $accent-color;
-						text-shadow: rem(2) rem(2) rem(1) $highlight-color;
-					}
-				}
+		}
 
-				.subtitle-text {
-					font-size: rem(24);
-				}
+		blockquote {
+			margin: rem(50) 0;
+			padding: rem(20);
+			font-style: italic;
+			border-left: rem(2) solid $accent-color;
+			font-size: rem(18);
+			color: var(--color-primary);
 
-				.call-to-action {
-					display: inline-block;
-					font-size: rem(30);
-					padding: rem(5) rem(30);
-
-					margin: 30px;
-
-					border-radius: rem(1);
-
-					box-shadow: rem(5) rem(5) 0 $accent-color, rem(-5) rem(-5) 0 $highlight-color;		
-					text-shadow: rem(2) rem(2) $highlight-color;
-
-					color: $accent-color;
-					font-family: $heading-font;
-					font-weight: bold;
-				}
-			}
-
-			.banner-scroll-indicator {
-				position: absolute;
-				display: flex;
-				flex-direction: column;
-				align-items: center;
-
-				gap: rem(18);
-
-				bottom: 0;
-				right: 50%;
-				transform: translateY(-50%) translateX(50%);
-
-				span {
-					$animation-execution-time: 1.5s;
-
-					position: relative;
-					width: var(--arrow-arm-length);
-
-					animation: bounce $animation-execution-time linear infinite;
-					animation-delay: var(--move-delay);
-
-					$arrow-arm-length: rem(20);
-					--arrow-arm-length: #{$arrow-arm-length};
-					--arm-color: #{$accent-color};
-					--move-delay: #{math.div($animation-execution-time, 4)};
-
-					&:first-of-type {
-						--arrow-arm-length: calc(#{$arrow-arm-length} * 0.85);
-						--move-delay: 0ms;
-					}
-
-					&:last-of-type {
-						--arrow-arm-length: calc(#{$arrow-arm-length} * 1.15);
-						--move-delay: #{math.div($animation-execution-time, 2)};
-					}
-
-					@keyframes bounce {
-						25% {
-							transform: scale(1);
-							opacity: 1;
-						}
-						50% {
-							transform: scale(1.2);
-							--arm-color: $highlight-color;
-							transform: scale(1.2) translateY(rem(2));
-						}
-						75% {
-							opacity: 1;
-						}
-					}
-					@keyframes color-shift {
-						15% {
-							background-color: mix($highlight-color, $accent-color, 50%);
-						}
-						50% {
-							background-color: $highlight-color;
-						}
-						85% {
-							background-color: mix($highlight-color, $accent-color, 50%);
-						}
-					}
-
-					&::after,
-					&::before {
-						content: '';
-						display: block;
-						width: var(--arrow-arm-length);
-						height: rem(2);
-						background-color: var(--arm-color);
-						border-radius: rem(10);
-						animation: color-shift $animation-execution-time linear infinite;
-						animation-delay: var(--move-delay);
-					}
-
-					&::after {
-						transform: translateY(-50%) translateX(-33%) rotateZ(45deg);
-					}
-
-					&::before {
-						transform: translateY(50%) translateX(33%) rotateZ(-45deg);
-					}
-				}
+			p {
+			width: 25ch;
 			}
 		}
 	}
